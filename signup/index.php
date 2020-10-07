5715d5204bd4aef00d25a1e30060be8b115bd27c
LaurynasAnd <l.andrulionis@gmail.com>
2020-10-07 16:04:45 +0300

    registration check

--- signup/index.php
+++ signup/index.php
@@ -1,19 +1,74 @@
 <?php
-require __DIR__.'/../data/addAccount.php';
-_log($_GET);
-_log($_POST);
-$check = 0;
-if(0 != count($_POST)){
+
+$check = 0; //this variable will count how many inputs are valid
+$responses = [
+    'wrongName' => '',
+    'wrongSurname' => '',
+    'badID' => '',
+    'pswMissmatch' => ''
+];
+if(0 != count($_POST)){ //patikrinama, ar post nera paliktas tuscias
 
     //check name
+    if(!preg_match_all('/[^\p{L}\p{M}*]+/u', $_POST['name']) && 0 != strlen($_POST['name'])){ // checks if there are only letters and returns 0
+        $check++;
+    } else {
+        $responses['wrongName'] = 'Netinkamo formato vardas';
+    }
+    
     
     //check surname
-    
-    //check id
-    
-    // check password
-    if(md5($_POST['psw']) == md5($_POST['repeatPsw'])){
+    if(!preg_match_all('/[^\p{L}\p{M}*]+/u', $_POST['surname']) && 0 != strlen($_POST['surname'])){ // checks if there are only letters and returns 0
         $check++;
+    } else {
+        $responses['wrongSurname'] = 'Netinkamo formato pavardė';
+    }
+    //check id Number
+    if(!preg_match_all('/[^\d]/', $_POST['idNumber']) && 11 == strlen($_POST['idNumber'])){ //preg_match_all will return 0 if there are only numbers in ID code. id number is made of 11 digits
+        //next two variables will check if gender digit and birth digits are valid
+        $isGender = false;  
+        $isDate = false;
+        $gender = intval(substr($_POST['idNumber'],0,1)); //returns int, first digit from ID
+        $birthDay = substr($_POST['idNumber'],1,6); //returns unformated date of birth
+        if ($gender > 2 && $gender <= 6) { // first digit is for gender and can be 3, 4, 5 or 6
+            $isGender = true;
+        }
+        //next the first two digits of the year will be determined. It can be done
+        //by checking the gender digit. 3-4 are from 1900s 5-6 are from 2000s
+        $year = (($gender == 3 || $gender == 4) ? '19' : '20') . substr($birthDay, 0, 2);
+        $date = $year . '-' . substr($birthDay, 2, 2) . '-' . substr($birthDay, 4, 2); //the full year is constructed in format YYYY-MM-dd
+        //the next function is used to validate the date. it return true if date is valid and false if date is not valid, for example 2020-10-01 is valid and 2020-13-01 is not
+        function validateDate($date, $format = 'Y-m-d'){
+            $d = DateTime::createFromFormat($format, $date);
+            // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
+            return $d && $d->format($format) === $date;
+        }
+        if(validateDate($date)){
+            $isDate = true;
+        }
+        if($isGender && $isDate){
+            $check++;
+        } else {
+            $responses['badID'] = 'Neteisingo formato asmens kodas';
+        }
+
+    } else {
+        $responses['badID'] = 'Neteisingai parašytas kodas';
+    }
+    // check password
+    if(md5($_POST['psw']) == md5($_POST['repeatPsw']) && (0 != strlen($_POST['psw']) && 0 != strlen($_POST['repeatPsw']))){ //paswords are not empty and are the same
+        $check++;
+    } else {
+        $responses['pswMissmatch'] = 'Nesutampa slaptažodžiai';
+    }
+
+
+    if(4 == $check){ //jei praeina visus patikrinimus
+        //prideti nauja vartotoja prie sistemos
+        require __DIR__.'/../data/addAccount.php';
+        _log('Registracija sėkminga');
+    } else {
+        //atmesti pildyma, ir parasyti, kas negerai parasyta buvo
     }
 }
 
@@ -41,10 +96,14 @@ if(0 != count($_POST)){
             <div class="infoscreen col-12">
                 <h1>Sveiki atvykę į LaBank</h1>
                 <p>Norėdami užsiregistruoti užpildykite žemiau esančius laukus. Visi laukai yra privalomi</p>
-                <form action="" method="post">
+                <form action="" method="post" accept-charset="utf-8">
+                    <div style="color:red; font-size:14px;"><?= $responses['wrongName']?></div>
                     <input type="text" name="name" id="name" placeholder="Vardas">
+                    <div style="color:red; font-size:14px;"><?= $responses['wrongSurname']?></div>
                     <input type="text" name="surname" id="surname" placeholder="Pavardė">
+                    <div style="color:red; font-size:14px;"><?= $responses['badID']?></div>
                     <input type="text" name="idNumber" id="idNumber" placeholder="Asmens kodas">
+                    <div style="color:red; font-size:14px;"><?= $responses['pswMissmatch']?></div>
                     <input type="password" name="psw" id="psw" placeholder="Slaptažodis">
                     <input type="password" name="repeatPsw" id="repeatPsw" placeholder="Pakartokite slaptažodį">
                     <button id="submit" type="submit">Atidaryti sąskaitą</button>
